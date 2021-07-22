b77dee66d25fe6935dc9b22e132246913def079d
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-07-22 22:20:22 +0200

    added navigation with router dom plus simple styles with css modules

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from './components/Home/Home'
-import About from './components/About/About'
-import SinglePost from './components/SinglePost/SinglePost'
-import Post from './components/Post/Post'
-import Project from './components/Project/Project'
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import SinglePost from "./components/SinglePost/SinglePost";
+import Post from "./components/Post/Post";
+import Project from "./components/Project/Project";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch> 
-        <Route component={Home} path='/' exact/>
-        <Route component={About} path='/about'/>
-        <Route component={SinglePost} path='/post/:slug'/>
-        <Route component={Post} path='/post'/>
-        <Route component={Project} path='/project'/>∑
+      <NavBar />
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />∑
       </Switch>
     </BrowserRouter>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./NavBar.modules.scss";
 
 export default function NavBar() {
-    return (
-        <div>
-                        
-
-        </div>
-    )
+  return (
+    <header className="header">
+      <div className="container">
+        <NavLink to="/" exact className='logo'>
+          Logo
+        </NavLink>
+        <nav className="navigation">
+          <NavLink to="/post" className='navLink'>Blog Posts</NavLink>
+          <NavLink to="/project" className='navLink'>Projects</NavLink>
+          <NavLink to="/about" className='navLink'>About me</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
