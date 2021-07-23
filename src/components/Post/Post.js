6759d85905197afa277949267286ef0891734168
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-07-23 11:31:35 +0200

    added queries for fetching date from Sanity in post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
 
 export default function Post() {
+  const [postData, setPostData] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type=="post"] {
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+        }
+      }`
+      )
+      .then((data) => setPostData(data))
+      .catch(console.error);
+  }, []);
   return (
-    <div>
-      <h1>post</h1>
-    </div>
+    <main>
+      <section>
+        <h1>Blog Posts Page</h1>
+        <h2>Welcome! to my page of blog posts</h2>
+        <div>
+          <article>
+            <Link>
+              <span>
+                <img />
+                <span>
+                  <h3></h3>
+                </span>
+              </span>
+            </Link>
+          </article>
+        </div>
+      </section>
+    </main>
   );
 }
